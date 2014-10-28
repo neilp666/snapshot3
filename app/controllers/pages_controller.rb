27edc826a4f5951b26f8b5987f47cf6003077451
neil patel <neil@myindianweddingapp.com>
2014-10-28 19:58:38 +0000

    Add Figaro gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,21 @@
 class PagesController < ApplicationController
   def home
+  	access_token = ENV["ACCESS_TOKEN"]
+  	config.client_id = ENV["CONFIG.CLIENT_ID"]
+  	config.client_secret = ENV["CONFIG.CLIENT_SECRET"]
+  	client = Instagram.client(access_token: access_token)
+  	default_search = client.tag_search('mustang')
+
+  	if params[:q]
+  		search_query = client.tag_search(params[:q])
+  		@tag = search_query.present? ? search_query : default_search
+  	else
+  		@tag = default_search
+  	end
+
+  	@tag = @tag.first.name
+  	@search = client.tag_recent_media(@tag)
+
   end
 
   def about
